251f877d411e17646207c5ca61ea3a70da9bbd54
Vinicius Otacilio <viniotacilio@gmail.com>
2021-06-15 18:40:38 -0300

    Corrigindo bug de componente nao importado

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import {  StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-// import { Container } from './styles';
 
-import { Login } from './pages/Login';
+import Login from './pages/Login';
 
 export default function App() {
     return (
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,34 +1,9 @@
-/*import React from 'react';
-import { Container } from './styles';
-import {  StyleSheet, Text, View } from 'react-native';
-
-const Login = () => {
-  return (
-    <Container style={styles.container}>
-      <Text>Safira</Text>
-      <StatusBar style="auto" />
-    </Container>
-  );
-}
-
-export default Login;
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#4181ba',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-});*/
-
 import React from 'react';
 import {  StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
-// import { Container } from './styles';
 
-export default function Login() {
+const Login = () => {
     return (
       <View style={styles.container}>
         <Text>Safira</Text>
@@ -44,3 +19,5 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
     },
 });
+
+export default Login;
