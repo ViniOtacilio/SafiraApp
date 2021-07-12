faf9ed34358f88382ffcaadeadba78d9121f240d
Bianca Ariga <biancaariga45@gmail.com>
2021-07-12 17:00:58 -0300

    Adicionada página de transações.

--- src/pages/RegisterTransactions/index.js
+++ src/pages/RegisterTransactions/index.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import {
   Container,
+  Header,
+  UserBox,
+  Title, 
+  Text,
+  Input,
+  SwitchSelectBox
 } from "./styles";
 import APIKit from "../../utils/APIKit";
 import { 
@@ -22,11 +28,71 @@ import {
 } from "@expo-google-fonts/roboto";
 import { AppLoading } from "expo";
 import { StyleSheet } from "react-native";
+import { 
+  SimpleLineIcons, 
+  Ionicons, 
+  FontAwesome,
+} from '@expo/vector-icons';
+import SwitchSelector from "react-native-switch-selector";
 
 class RegisterTransactions extends Component {
   render() {
     return (
       <Container>
+        <Header>
+          <UserBox>
+            <FontAwesome name="user-circle" size={26} color="#FAFAFF" />
+            <Title>Olá, Fulano!</Title>
+          </UserBox>
+          <SimpleLineIcons name="menu" size={24} color="#FAFAFF" />
+        </Header>
+        <Text>
+          Saldo: R$5000,00
+        </Text>
+        <SwitchSelectBox>
+          <SwitchSelector
+            initial={0}
+            onPress={value => this.setState({ entrance_exit: value })}
+            textColor={'#E4D9FF'}
+            selectedColor={'#30343F'}
+            buttonColor={'#E4D9FF'}
+            borderColor={'#E4D9FF'}
+            hasPadding
+            options={[
+              { label: "Entrada", value: "entrada" }, 
+              { label: "Saída", value: "saida" } 
+            ]}
+            borderRadius={4}
+            fontSize={18}
+          />
+        </SwitchSelectBox>
+        <Input
+            placeholder="Valor"
+        ></Input>
+        <Input
+            placeholder="Descrição"
+        ></Input>
+        <Input
+            placeholder="Data"
+        ></Input>
+        <SwitchSelectBox>
+          <SwitchSelector
+            initial={0}
+            onPress={value => this.setState({ release: value })}
+            textColor={'#E4D9FF'}
+            selectedColor={'#30343F'}
+            buttonColor={'#E4D9FF'}
+            borderColor={'#E4D9FF'}
+            hasPadding
+            options={[
+              { label: "Fixa", value: "fixa" }, 
+              { label: "Parcelada", value: "parcelada" } 
+            ]}
+            borderRadius={4}
+            fontSize={18}
+          />
+        </SwitchSelectBox>
+        <SimpleLineIcons name="check"  size={44} color="#FAFAFF" style={{textAlign: 'center', cursor: 'pointer'}}  />
       </Container>
     );
   }
--- src/pages/RegisterTransactions/styles.js
+++ src/pages/RegisterTransactions/styles.js
@@ -6,3 +6,46 @@ export const Container = styled.View`
     padding: 60px 20px;
     justify-content: space-between;
 `;
+
+export const Header = styled.View`
+    flex: 1;
+    background: #1E2749;
+    flex-direction: row;
+    justify-content: space-between;
+    max-height: 30px;
+`;
+
+export const UserBox = styled.View`
+    display: flex;
+    flex-direction: row;
+    max-height: 30px;
+`;
+
+export const Title = styled.Text`
+    color: #FAFAFF;
+    font-size: 20px;
+    margin: 0 8px;
+`;
+
+export const Text = styled.Text`
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center; 
+    color: #FAFAFF; 
+`;
+
+export const Input = styled.TextInput`
+    background: #FAFAFF; 
+    height: 40px;
+    width: 80%;
+    border-radius: 4px;
+    padding: 8px;
+    font-size: 18px;
+    text-align: center;
+    margin: 0 auto;
+`;
+
+export const SwitchSelectBox = styled.View`
+    width: 80%;
+    margin: 0 auto;
+`;
